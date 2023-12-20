ed8da9a6e3e5c2f4a24634c8abddc61cc69fd768
Craig <CMPeoples3@gmail.com>
2023-12-20 09:51:31 -0800

    Update go script with minor improvements

--- Go/main.go
+++ Go/main.go
@@ -38,7 +38,6 @@ var wcCmd = &cobra.Command{
 		for _, arg := range args {
 			// Perform actions with the variadic strings here
 			// filePaths = append(filePaths, arg)
-			fmt.Println("Files:", arg, configValue.Words)
 			processFile(&c, &configValue, arg)
 		}
 	},
@@ -70,7 +69,7 @@ func processFile(c *countStruct, cfg *configStruct, filePath string) error {
 			count := len(words)
 			c.Words += int32(count)
 		}
-		if cfg.Characters {
+		if cfg.Characters && !cfg.Count {
 			// Count characters as all characters not counting spaces,
 			c.Characters += int64(len(strings.Join(words, " ")))
 		}
@@ -80,15 +79,21 @@ func processFile(c *countStruct, cfg *configStruct, filePath string) error {
 		return err
 	}
 
-	fmt.Printf("%d %d %d %d\n", c.Count, c.Lines, c.Words, c.Characters)
+	fmt.Printf("%d %d %d %s\n", c.Lines, c.Words, func() int64 {
+		if cfg.Count {
+			return c.Count
+		} else {
+			return c.Characters
+		}
+	}(), filePath)
 
 	return nil
 }
 
 func main() {
-	wcCmd.Flags().BoolVarP(&configValue.Count, "count", "c", false, "The number of bytes in each input file is written to the standard output. This will cancel out any prior usage of the -m option.")
+	wcCmd.Flags().BoolVarP(&configValue.Count, "count", "c", false, "The number of bytes in each input file is written to the standard output. This will cancel out any usage of the -m option.")
 	wcCmd.Flags().BoolVarP(&configValue.Lines, "lines", "l", false, "The number of lines in each input file is written to the standard output.")
-	wcCmd.Flags().BoolVarP(&configValue.Characters, "characters", "m", false, "The number of characters in each input file is written to the standard output. If the current locale does not support multibyte characters, this is equivalent to the -c option. This will cancel out any prior usage of the -c option.")
+	wcCmd.Flags().BoolVarP(&configValue.Characters, "characters", "m", false, "The number of characters in each input file is written to the standard output. If the current locale does not support multibyte characters, this is equivalent to the -c option.")
 	wcCmd.Flags().BoolVarP(&configValue.Words, "words", "w", false, "The number of words in each input file is written to the standard output.")
 	// wcCmd.Flags().StringSliceVarP(&filePaths, "files", "f", []string{}, "Input file paths")
 
